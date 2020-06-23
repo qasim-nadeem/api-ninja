8f138cfdb39b2cf73fdcf63e5fbefedc1ab4e87f
qasim-nadeem <qqnadeem@gmail.com>
2020-06-24 00:30:26 +0500

    added migration to create apps table.

---
+++ database/migrations/2020_06_23_191640_create_apps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apps', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('user_session_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('token')->nullable();
+            $table->timestamps();
+        });
+        Schema::table('apps', function($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apps');
+    }
+}
